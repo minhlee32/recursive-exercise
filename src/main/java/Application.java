9a21eca53d7c5ee7067a3b32e83704a1d9033806
Minh Lee <minhlee.fre.mta@gmail.com>
2018-07-18 23:43:02 +0700

    add Application

---
+++ src/main/java/Application.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) throws IOException {
+        System.out.println("The program shows the directory tree");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the path of directory: ");
+        String path = scanner.nextLine();
+
+        directoryTree(path);
+
+
+    }
+
+    public static void directoryTree(String path) throws IOException {
+        File dir = new File(path);
+        if (!dir.exists()) {
+            System.out.println("Folder " + path + " does not exist!");
+        }
+        if (dir.isDirectory()) {
+            File[] list = dir.listFiles();
+            if (list != null) {
+                for (File f : list) {
+                    System.out.println(f.getCanonicalPath());
+                    directoryTree(f.getCanonicalPath());
+
+                }
+            }
+        }
+    }
+}
